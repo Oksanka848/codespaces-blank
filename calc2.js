e7414729cd797fc87d492c04b51e2cc64c56d3d0
Oksanaka848 <okmitka@mail.ru>
2022-11-26 18:27:07 +0300

    ch

--- calc2.js
+++ calc2.js
@@ -5,29 +5,19 @@
  // button.onclick = ButtonClick;
  let n1 = document.getElementById('n1').value;
  let n2 = document.getElementById('n2').value;
- let operationButtons = document.querySelectorAll('[operation]');
-
-class Calculator {
+ //let operation = document.querySelectorAll('[operation]');
+ let button = document.getElementById("button");
+let op;
+ class Calculator {
     constructor(n1, n2) {
       this.n1 = n1; //число 1
       this.n2 = n2; //число 2 
-    }
-    chooseOperation(operation) {
-      if (this.n1 === '') return
-      if (this.n2 !== '') {
-        this.ButtonClick()
-      }
-      this.operation = operation
-      this.n1 = this.n2
-      this.n1 = ''
-    }
-
+    }  
+    
     ButtonClick() {
-      let result
-      const n1 = parseFloat(this.n1)
-      const n2 = parseFloat(this.n2)
-      if (isNaN(n1) || isNaN(n2)) return
-      switch (this.operation) {
+      let result;
+      
+      switch (result) {
         case '+':
           result = n1 + n2
           break
@@ -43,16 +33,16 @@ class Calculator {
         default:
           return
       }
-      
     }
-}
-  const calculator = new Calculator(n1, n2);
+    }
+
+  let calculator = new Calculator(n1, n2);
   
-    operationButtons.addEventListener('click', () => {
-      calculator.ButtonClick()
+  document.getElementById("result").innerHTML=result;
+/*  button.addEventListener('click',calculator
       
-    });
-  
+    );*/
+ 
   
   //let plus = document.getElementById("plus");
   //let v= plus.addEventListener('click',Calculator.ButtonClick2());
--- calc3.js
+++ calc3.js
@@ -1,43 +1,41 @@
-let n1 = document.getElementById('n1').value;
- let n2 = document.getElementById('n2').value;
- let operationButtons = document.querySelectorAll('[operation]');
+/*Создайте класс Калькулятор, который будет содержать 4 статических метода 
+- Сложение, Умножение, Вычитание и Деление. 
+Доработайте свою страничку с калькулятором, 
+чтобы в ней использовались методы этого класса.*/
+let n1 = document.getElementById("n1").value;
+let n2 = document.getElementById("n2").value;
+let operation = document.querySelectorAll("[operation]");
 
 class Calculator {
-    constructor(n1, n2) {
-      this.n1 = n1; //число 1
-      this.n2 = n2; //число 2 
-    }
-    
-
-    ButtonClick() {
-      let plus = document.getElementById('result');
-      plus.innerHTML+=`Результат вычисления = ${this.n1 + this.n2}`;
-    }
-
-    ButtonClick2() {
-        console.log(`Результат вычисления = ${this.n1 - this.n2}`);
-    }
-
-    ButtonClick3() {
-        console.log(`Результат вычисления = ${this.n1 * this.n2}`);
-    }
-
-    ButtonClick4() {
-        if (n2 == 0) {
-            alert(`На ноль делить нельзя`);
-          }
-        console.log(`Результат вычисления = ${this.n1 / this.n2}`);
-    }
-    
+  constructor(n1, n2) {
+    this.n1 = +n1; //число 1
+    this.n2 = +n2; //число 2
   }
-  
-  let calculator = new Calculator(n1, n2);
-  operationButtons.forEach(button => {
-    button.addEventListener('click', () => {
-      calculator.ButtonClick()
-      calculator.ButtonClick2()
-      calculator.ButtonClick3()
-      calculator.ButtonClick4()
-    })
-  })
-  
+
+  ButtonClick() {
+    console.log(`Результат вычисления = ${this.n1 + this.n2}`);
+  }
+
+  ButtonClick2() {
+    console.log(`Результат вычисления = ${this.n1 - this.n2}`);
+  }
+
+  ButtonClick3() {
+    console.log(`Результат вычисления = ${this.n1 * this.n2}`);
+  }
+
+  ButtonClick4() {
+    if (n2 == 0) {
+      console.log(`На ноль делить нельзя`);
+    }
+    console.log(`Результат вычисления = ${this.n1 / this.n2}`);
+  }
+}
+
+let calculator = new Calculator(n1, n2);
+
+calculator.ButtonClick();
+calculator.ButtonClick2();
+calculator.ButtonClick3();
+calculator.ButtonClick4();
+let button = document.addEventListener('click')
